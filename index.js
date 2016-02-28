2a13f80835a95c6bd103858c30efdfd186ab6df6
davidmarkclements <huperekchuno@googlemail.com>
2016-02-28 19:45:26 +0000

    v4

--- index.js
+++ index.js
@@ -1,213 +1,216 @@
 'use strict'
 
 var fs = require('fs');
-var HISTORY_MAX = 100;
-var HISTORY_FILE = '.prompt_hist.txt'
-var HIST = [];
+var term = 13; // carriage return
 
 /**
- * prompt -- sync function for reading user input from stdin
- * @param   {Object} option {
- *   hidden: If true, user input will not be echoed,
- *   echo: set to a character to be echoed, default is '*'. Use '' for no echo
- *   tabComplete: {StringArray} function({String}) 
- *   value: {String} initial value for the prompt
+ * create -- sync function for reading user input from stdin
+ * @param   {Object} config {
  *   sigint: {Boolean} exit on ^C
- *   ask: {String} opening question/statement to prompt for
+ *   autocomplete: {StringArray} function({String})
+ *   history: {String} a history control object (see `prompt-sync-history`)
  * }
- * @returns {string} Returns the string input or (if sigint === false) 
- *                   null if user terminates with a ^C 
+ * @returns {Function} prompt function 
  */
 
-function prompt(option) {
-  
-  var term = 13; // carriage return
-  var hidden = false;
-  var histindex = HIST.length;
-  var insert = 0, savedinsert = 0, res, i, savedstr;
+function create(config) {
 
-  option = option || {};
-  option.tabComplete = option.tabComplete || function(){return []} ;
+  config = config || {};
+  var sigint = config.sigint;
+  var autocomplete = config.autocomplete = 
+    config.autocomplete || function(){return []};
+  var history = config.history;
+  prompt.history = history || {save: function(){}};
+  prompt.hide = function (ask) { return prompt(ask, {echo: ''}) };
 
-  if (option && option.hidden) {
-    hidden = true;
-    if (!option.hasOwnProperty('echo'))
-      option.echo = '*';
-  }
+  return prompt;
 
-  var fd = (process.platform === 'win32') ? 
-    process.stdin.fd :
-    fs.openSync('/dev/tty', 'rs')
 
-  var wasRaw = process.stdin.isRaw;
-  if (!wasRaw) { process.stdin.setRawMode(true); }
+  /**
+   * prompt -- sync function for reading user input from stdin
+   *  @param {String} ask opening question/statement to prompt for
+   *  @param {String} value initial value for the prompt
+   *  @param   {Object} opts {
+   *   echo: set to a character to be echoed, default is '*'. Use '' for no echo
+   *   value: {String} initial value for the prompt
+   *   ask: {String} opening question/statement to prompt for, does not override ask param
+   *   autocomplete: {StringArray} function({String})
+   * }
+   * 
+   * @returns {string} Returns the string input or (if sigint === false) 
+   *                   null if user terminates with a ^C  
+   */
 
-  var buf = new Buffer(3);
-  var str = '', char, read;
-  
-  savedstr = '';
 
-  if (option.ask) {
-    insert = option.ask.length;
-    process.stdout.write(option.ask);
-  }
+  function prompt(ask, value, opts) {
+    var insert = 0, savedinsert = 0, res, i, savedstr;
+    opts = opts || {};
 
-  if (option.value) {
-    insert += option.value.length;
-    str = option.value;
-    process.stdout.write(option.value);
-  }
+    if (Object(ask) === ask) {
+      opts = ask;
+      ask = opts.ask;
+    } else if (Object(value) === value) {
+      opts = value;
+      value = opts.value;
+    }
+    ask = ask || '';
+    var echo = opts.echo;
+    var masked = 'echo' in opts;
+    autocomplete = opts.autocomplete || autocomplete;
 
-  var cycle = 0;
-  var prevComplete;
+    var fd = (process.platform === 'win32') ? 
+      process.stdin.fd :
+      fs.openSync('/dev/tty', 'rs');
 
-  while (true) {
-    read = fs.readSync(fd, buf, 0, 3);
-    //process.stdout.write('read '+ read + ' '+ buf[0]);
-    if (read == 3) { // received a control sequence
-      switch(buf.toString()) {
+    var wasRaw = process.stdin.isRaw;
+    if (!wasRaw) { process.stdin.setRawMode(true); }
+
+    var buf = new Buffer(3);
+    var str = '', char, read;
+    
+    savedstr = '';
+
+    if (ask) {
+      process.stdout.write(ask);
+    }
+
+    var cycle = 0;
+    var prevComplete;
+
+    while (true) {
+      read = fs.readSync(fd, buf, 0, 3);
+      if (read == 3) { // received a control sequence
+        switch(buf.toString()) {
           case '\u001b[A':  //up arrow
-            if (hidden) break;
-            if (histindex <= 0) break;
-            if (histindex == HIST.length) {
+            if (masked) break;
+            if (!history) break;
+            if (history.atStart()) break;
+            
+            if (history.atEnd()) {
               savedstr = str;
               savedinsert = insert;
             }
-            str = HIST[--histindex];
+            str = history.prev();
             insert = str.length;
-            process.stdout.write("\u001b[2K\u001b[0G" +  str);
+            process.stdout.write('\u001b[2K\u001b[0G' + ask + str);
             break;
           case '\u001b[B':  //down arrow
-            if (hidden) break;
-            if (histindex >= HIST.length) break;
-            if (histindex == HIST.length -1) {
+            if (masked) break;
+            if (!history) break;
+            if (history.pastEnd()) break;
+            
+            if (history.atPenultimate()) {
               str = savedstr;
               insert = savedinsert;
-              histindex++;
+              history.next();
             } else {
-              str = HIST[++histindex];
+              str = history.next();
               insert = str.length;
             }
-            process.stdout.write("\u001b[2K\u001b[0G"+ str + "\u001b["+(insert+1)+"G");
+            process.stdout.write('\u001b[2K\u001b[0G'+ ask + str + '\u001b['+(insert+ask.length+1)+'G');
             break;
           case '\u001b[D': //left arrow
-            if (hidden) break;
+            if (masked) break;
+            var before = insert;
             insert = (--insert < 0) ? 0 : insert;
-            process.stdout.write("\u001b[1D" +  '');
+            if (before - insert)
+              process.stdout.write('\u001b[1D');
             break;
           case '\u001b[C': //right arrow
-            if (hidden) break;
+            if (masked) break;
             insert = (++insert > str.length) ? str.length : insert;
-            process.stdout.write("\u001b[" + (insert+1) + "G");
+            process.stdout.write('\u001b[' + (insert+ask.length+1) + 'G');
             break;
-      }
-      continue; // any other 3 character sequence is ignored
-    }
-    
-    // if it is not a control character seq, assume only one character is read
-    char = buf[read-1];
-    
-    // catch a ^C and return null
-    if (char == 3){ 
-      process.stdout.write('^C\n');
-      fs.closeSync(fd);
-      
-      if (option.sigint)
-        process.exit(130);
-
-      process.stdin.setRawMode(wasRaw);
-
-      return null;
-    }
-
-    // catch the terminating character
-    if (char == term) {
-      if (!hidden && str.length)
-        HIST.push(str);
-      fs.closeSync(fd);
-      break;
-    }
-    
-    // catch a TAB and implement tabcomplete
-    if (char == 9) { // TAB
-      res = option.tabComplete(str);
-
-      if (str == res[0]) {
-        res = option.tabComplete('');
-      }
-      else {
-        prevComplete = res.length;
-      }
-
-      if (res.length == 0) {
-        process.stdout.write("\t");
-        continue;
-      }
-
-      var item = res[cycle++] || res[cycle = 0, cycle++]
-
-      if (item) {
-        process.stdout.write("\r\u001b[K" + item);
-        str = item;
-        insert = item.length;
-      }
-    }
-    
-    if (char == 127) { //backspace
-      if (!insert) continue;
-      str = str.slice(0, insert-1) + str.slice(insert);
-      insert--;
-    } else {
-      if ((char < 32 ) || (char > 126))
-          continue;
-      str = str.slice(0, insert) + String.fromCharCode(char) + str.slice(insert);
-      insert++;
-    };
-    
-    if (hidden) {
-        process.stdout.write("\u001b[2K\u001b[0G" +  Array(str.length+1).join(option.echo));
-    } else {
-      if (insert == str.length) {
-        process.stdout.write("\u001b[2K\u001b[0G"+ str);
-      } else {
-
-        if (option.ask) {
-          process.stdout.write("\u001b[2K\u001b[0G"+ option.ask + str);
-        } else {
-          process.stdout.write("\u001b[2K\u001b[0G"+ str + "\u001b[" + (str.length-insert) +"D" );
         }
-        
+        continue; // any other 3 character sequence is ignored
       }
-    }    
-  }
-  
-  process.stdout.write('\n')
+      
+      // if it is not a control character seq, assume only one character is read
+      char = buf[read-1];
+      
+      // catch a ^C and return null
+      if (char == 3){ 
+        process.stdout.write('^C\n');
+        fs.closeSync(fd);
+        
+        if (sigint) process.exit(130);
 
-  process.stdin.setRawMode(wasRaw);
-  
-  return str;
+        process.stdin.setRawMode(wasRaw);
+
+        return null;
+      }
+
+      // catch the terminating character
+      if (char == term) {
+        fs.closeSync(fd);
+        if (!history) break;
+        if (!masked && str.length) history.push(str);
+        history.reset();
+        break;
+      }
+      
+      // catch a TAB and implement autocomplete
+      if (char == 9) { // TAB
+        res = autocomplete(str);
+
+        if (str == res[0]) {
+          res = autocomplete('');
+        } else {
+          prevComplete = res.length;
+        }
+
+        if (res.length == 0) {
+          process.stdout.write('\t');
+          continue;
+        }
+
+        var item = res[cycle++] || res[cycle = 0, cycle++];
+
+        if (item) {
+          process.stdout.write('\r\u001b[K' + ask + item);
+          str = item;
+          insert = item.length;
+        }
+      }
+      
+      if (char == 127) { //backspace
+        if (!insert) continue;
+        str = str.slice(0, insert-1) + str.slice(insert);
+        insert--;
+        process.stdout.write('\u001b[2D');
+      } else {
+        if ((char < 32 ) || (char > 126))
+            continue;
+        str = str.slice(0, insert) + String.fromCharCode(char) + str.slice(insert);
+        insert++;
+      };
+      
+      
+      if (masked) {
+          process.stdout.write('\u001b[2K\u001b[0G' + ask + Array(str.length+1).join(echo));
+      } else {
+        process.stdout.write('\u001b[s');
+        if (insert == str.length) {
+            process.stdout.write('\u001b[2K\u001b[0G'+ ask + str);
+        } else {
+          if (ask) {
+            process.stdout.write('\u001b[2K\u001b[0G'+ ask + str);
+          } else {
+            process.stdout.write('\u001b[2K\u001b[0G'+ str + '\u001b[' + (str.length - insert) + 'D');
+          }
+        }
+        process.stdout.write('\u001b[u');
+        process.stdout.write('\u001b[1C');
+      }
+
+    }
+    
+    process.stdout.write('\n')
+
+    process.stdin.setRawMode(wasRaw);
+    
+    return str || value || '';
+  };
 };
 
-
-function init(history_file, history_max) {
-  HISTORY_MAX = history_max || HISTORY_MAX;
-  HISTORY_FILE = history_file || HISTORY_FILE;
-  try {
-    HIST = fs.readFileSync(HISTORY_FILE, {encoding: 'utf8'});
-    HIST = HIST.split('\n').slice(0,-1);
-  } catch(e) {
-    HIST = [];
-  }
-  HIST = HIST.slice(HIST.length-HISTORY_MAX, HIST.length);
-}
-
-function save(){
-    fs.writeFileSync(HISTORY_FILE, HIST.join('\n') + '\n');
-}
-
-module.exports = {
-  prompt: prompt,
-  save: save,
-  init: init
-}
-
+module.exports = create;
